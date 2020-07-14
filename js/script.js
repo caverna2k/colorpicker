8b8e33b5f544c53f2d88be20707f9e32483d4103
caverna2k <daniel.rubel@gmail.com>
2020-07-13 21:25:11 -0300

    commit of working Color Picker

--- js/script.js
+++ js/script.js
@@ -1,96 +1,68 @@
+window.addEventListener('load', start);
+
 console.log('JavaScript em funcionamento!');
 
-// Comparando 2 números com if/else
-var a = 3;
-var b = 5;
+var redSlider = document.getElementById('red');
+var greenSlider = document.getElementById('green');
+var blueSlider = document.getElementById('blue');
+var colorDiv = document.getElementById('pickedcolor');
+var hexval = document.getElementById('hexval');
+var hexDigits = new Array(
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f'
+);
 
-if (a < b) {
-  console.log(a + ' é menor que ' + b);
-} else {
-  if (a > b) {
-    console.log(a + ' é maior que ' + b);
-  } else {
-    console.log(a + ' e ' + b + ' são iguais');
-  }
+function start() {
+  var events = 'input';
+
+  addChangeEventstoElement(redSlider, events, handleChange);
+  addChangeEventstoElement(greenSlider, events, handleChange);
+  addChangeEventstoElement(blueSlider, events, handleChange);
+  addChangeEventstoElement(redSlider, events, updateBitOnChange);
+  addChangeEventstoElement(greenSlider, events, updateBitOnChange);
+  addChangeEventstoElement(blueSlider, events, updateBitOnChange);
 }
 
-// Validação de dia com if/else
-var day = 1;
-
-if (day === 1) {
-  console.log('Domingo');
-} else {
-  if (day === 2) {
-    console.log('Segunda-feira');
-  } else {
-    if (day === 3) {
-      console.log('Terça-feira');
-    } else {
-      if (day === 4) {
-        console.log('Quarta-feira');
-      } else {
-        if (day === 5) {
-          console.log('Quinta-feira');
-        } else {
-          if (day === 6) {
-            console.log('Sexta-feira');
-          } else {
-            if (day === 7) {
-              console.log('Sábado');
-            } else {
-              console.log('Dia inválido');
-            }
-          }
-        }
-      }
-    }
-  }
+function addChangeEventstoElement(element, events, func) {
+  events.split(' ').forEach((e) => element.addEventListener(e, func, false));
 }
 
-// Validação de dia com switch
-switch (day) {
-  case 1:
-    console.log('Domingo');
-    break;
-  case 2:
-    console.log('Segunda-feira');
-    break;
-  case 3:
-    console.log('Terça-feira');
-    break;
-  case 4:
-    console.log('Quarta-feira');
-    break;
-  case 5:
-    console.log('Quinta-feira');
-    break;
-  case 6:
-    console.log('Sexta-feira');
-    break;
-  case 7:
-    console.log('Sábado');
-    break;
-  default:
-    console.log('Dia inválido');
+function handleChange() {
+  r = redSlider.value;
+  g = greenSlider.value;
+  b = blueSlider.value;
+
+  colorDiv.style.backgroundColor = rgb(r, g, b);
+  hexval.value = rgb2hex(colorDiv.style.backgroundColor);
 }
 
-// Comparando 2 números com operador ternário
-var compareResult = a > b ? 1 : a < b ? -1 : 0;
+function rgb(r, g, b) {
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
 
-// Verificando dia da semana com operador ternário
-var weekDay =
-  day === 1
-    ? 'Domingo'
-    : day === 2
-    ? 'Segunda-feira'
-    : day === 3
-    ? 'Terça-feira'
-    : day === 4
-    ? 'Quarta-feira'
-    : day === 5
-    ? 'Quinta-feira'
-    : day === 6
-    ? 'Sexta-feira'
-    : day === 7
-    ? 'Sábado'
-    : 'Dia inválido';
+function updateBitOnChange() {
+  document.getElementById(this.id + 'Bit').value = this.value;
+}
+
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function hex(x) {
+  return isNaN(x) ? '00' : hexDigits[(x - (x % 16)) / 16] + hexDigits[x % 16];
+}
